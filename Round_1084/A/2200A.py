ee1951e9822c8023ef954c4b9ab9b44f5647e0a7
tjdms4327 <tjdms4327@gmail.com>
2026-04-16 22:58:49 +0900

    Create 2200A.py

---
+++ Round_1084/A/2200A.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    A = list(map(int, input().split()))
+    
+    MAX = max(A)
+    print(A.count(MAX))
