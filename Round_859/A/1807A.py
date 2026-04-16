aee25734fa74b5acdbda319aaea2bf4f05c90e51
tjdms4327 <tjdms4327@gmail.com>
2026-04-16 22:35:31 +0900

    Create 1807A.py

---
+++ Round_859/A/1807A.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    a, b, c = map(int, input().split())
+
+    if a+b == c:
+        print('+')
+    else:
+        print('-')
