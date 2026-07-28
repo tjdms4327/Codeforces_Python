83a0d05e78b27b7735558daf5483aca65b69b15c
tjdms4327 <tjdms4327@gmail.com>
2026-07-28 23:48:58 +0900

    Add files via upload

---
+++ Round981/Div.3/2033A.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    
+    if n % 2 :
+        print('Kosuke')
+    else:
+        print('Sakurako')
