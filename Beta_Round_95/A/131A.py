558746a7a32b9a323f22f1358d6510c5367e8f7f
tjdms4327 <tjdms4327@gmail.com>
2026-07-31 20:39:51 +0900

    Delete Beta_Round_95/A directory

--- Beta_Round_95/A/131A.py
+++
@@ -1,9 +0,0 @@
-s = input().strip()
-
-if len(s)==1:
-    print(s.swapcase())
-else:
-    if s.isupper() or (s[0].islower() and s[1:].isupper()):
-        print(s.swapcase())
-    else:
-        print(s)
