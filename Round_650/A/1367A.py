38894780d74cbcfb52658782e4232a3c0d330059
tjdms4327 <tjdms4327@gmail.com>
2026-08-13 21:42:50 +0900

    Delete Round_650 directory

--- Round_650/A/1367A.py
+++
@@ -1,9 +0,0 @@
-import sys, math
-input = sys.stdin.readline
-
-t = int(input())
-for _ in range(t):
-    b = input().strip()
-    a = [b[0]] + [b[i] for i in range(1, len(b), 2)]
-    
-    print(''.join(a))
